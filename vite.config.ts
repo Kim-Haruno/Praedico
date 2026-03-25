974e5613a56ae55da2261f9805ea934732250a63
MxNeil <mcneal0745516650@gmail.com>
2026-03-25 15:50:40 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()],
-  base: "/Praedico",
+  base: "/Praedico/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
